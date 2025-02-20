63f71d49d0e8822821e5fcdee9978bd9a839f546
Bromolima <bromolima@gmail.com>
2025-02-20 16:23:47 -0300

    Front-end template created

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='flex justify-center items-center w-screen h-screen bg-gray-800'>
+          <p className='text-white text-5xl'>Hellow World</p>
+      </div>
+    </>
+  )
+}
+
+export default App
